cf03555b349485469dd6bd6dfbd21be0af34a225
zodiakara <114649190+zodiakara@users.noreply.github.com>
2022-11-24 00:32:54 +0100

    selected state added

--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -1,14 +1,34 @@
 import Card from "react-bootstrap/Card";
+import { Component } from "react";
 
-function SingleBook({ book }) {
-  return (
-    <Card style={{ width: "18rem" }}>
-      <Card.Img variant="top" src={book.img} />
-      <Card.Body>
-        <Card.Title>{book.title}</Card.Title>
-      </Card.Body>
-    </Card>
-  );
+class SingleBook extends Component {
+  state = {
+    selected: false,
+  };
+
+  handleOnClick() {
+    this.setState({ selected: !this.state.selected });
+  }
+
+  render() {
+    const book = this.props.book;
+    const isSelected = this.state.selected ? "lightgrey" : "";
+
+    return (
+      <Card
+        onClick={this.handleOnClick}
+        style={{
+          width: "18rem",
+          backgroundColor: isSelected,
+        }}
+      >
+        <Card.Img variant="top" src={book.img} />
+        <Card.Body>
+          <Card.Title>{book.title}</Card.Title>
+        </Card.Body>
+      </Card>
+    );
+  }
 }
 
 export default SingleBook;
